87dc3b1f8aaed7532ebc85d0636de3bd452bf46e
1730189 <55115762+1730189@users.noreply.github.com>
2020-06-14 22:49:00 -0500

    Web

--- Models/conexion.php
+++ Models/conexion.php
@@ -3,7 +3,7 @@
 	class Conexion{
 
 		public static function conectar(){
-			$link = new PDO("mysql:host=localhost;dbname=practica25052020-inventario", "root", "");
+			$link = new PDO("mysql:host=localhost;dbname=practica25052020-inventario", "admin", "d10e54838b7899432ed12e60b9d00f82afd9d012bc7e97c5");
 			return $link;
 		}
 	}
